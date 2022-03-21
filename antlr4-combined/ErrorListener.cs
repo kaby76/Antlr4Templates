1043fc07f2fd169741c5e0c74ebe0abb712346e8
Ken Domino <kenneth.domino@domemtech.com>
2022-03-21 14:56:48 -0400

    Fix.

--- antlr4-combined/ErrorListener.cs
+++ antlr4-combined/ErrorListener.cs
@@ -1,22 +1,22 @@
 ﻿// Template generated code from Antlr4Templates v1.1
 namespace TemplateNamespace
 {
-    using Antlr4.Runtime;
-    using Antlr4.Runtime.Misc;
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
+	using Antlr4.Runtime;
+	using Antlr4.Runtime.Misc;
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
 
-    public class ErrorListener : Antlr4.Runtime.BaseErrorListener
-    {
-        public bool had_error;
+	public class ErrorListener<S> : ConsoleErrorListener<S>
+	{
+		public bool had_error;
 
-        public override void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg,
-            RecognitionException e)
-        {
-            had_error = true;
-            base.SyntaxError(recognizer, offendingSymbol, line, charPositionInLine, msg, e);
-        }
-    }
+		public override void SyntaxError(TextWriter output, IRecognizer recognizer, S offendingSymbol, int line,
+			int col, string msg, RecognitionException e)
+		{
+			had_error = true;
+			base.SyntaxError(output, recognizer, offendingSymbol, line, col, msg, e);
+		}
+	}
 }
